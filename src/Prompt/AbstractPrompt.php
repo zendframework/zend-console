c8483c4c88bc563eb5dd00f96268983770a2159d
Artur Bodera <abodera@gmail.com>
2012-02-20 17:43:24 +0100

    Refactor Cli to Console, add route matching test stub

---
+++ src/Prompt/AbstractPrompt.php
@@ -0,0 +1,54 @@
+<?php
+namespace Zend\Console\Prompt;
+
+use Zend\Console\Prompt,
+Zend\Console\Console,
+Zend\Console\Adapter as ConsoleAdapter
+;
+
+abstract class AbstractPrompt implements Prompt
+{
+    /**
+     * @var \Zend\Console\Adapter
+     */
+    protected $console;
+
+    /**
+     * @var mixed
+     */
+    protected $lastResponse;
+
+    public function show(){}
+
+    /**
+     * Return last answer to this prompt.
+     *
+     * @return mixed
+     */
+    public function getLastResponse(){
+        return $this->lastResponse;
+    }
+
+    /**
+     * Return console adapter to use when showing prompt.
+     *
+     * @return \Zend\Console\Adapter
+     */
+    public function getConsole(){
+        if(!$this->console){
+            $this->console = Console::getInstance();
+        }
+
+        return $this->console;
+    }
+
+    /**
+     * Set console adapter to use when showing prompt.
+     *
+     * @param \Zend\Console\Adapter    $adapter
+     */
+    public function setConsole(ConsoleAdapter $adapter){
+        $this->console = $adapter;
+    }
+
+}
